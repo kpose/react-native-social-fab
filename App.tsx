78069e805c18042d135778c908933e6d02ca072e
kpose <ganihujude@gmail.com>
2021-09-23 16:38:31 +0100

    total verhaul to bare RN

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <Text>welcome</Text>
+    </View>
+  );
+};
+
+export default App;
